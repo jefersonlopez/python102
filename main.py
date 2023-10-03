2e11f5f1544adfc88fc2d8e9a3961b26373b4b68
jefersonlopez <jeferson_andrey@outlook.com>
2023-10-03 18:35:46 -0500

    comprehensions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 '''
 # conjuntos
 
@@ -30,7 +32,7 @@ print(set_comuntries)
 
 set_comuntries.clear()
 print(set_comuntries)
-'''
+
 
 #operaciones de conjuntos
 set_a = {'col', 'mex', 'bol'}
@@ -49,3 +51,65 @@ print(set_c)
 
 set_c = set_a.symmetric_difference(set_b)
 print(set_c)
+
+#list comprehensions 
+
+numbers = []
+for element in range(1,12):
+    numbers.append(element)
+
+print(numbers)
+
+numbers_2 = [element for element in range(1,15)]
+print(numbers_2)
+
+numbers_3 = [element for element in range(1,15) if element % 2 == 0]
+print(numbers_3)
+
+
+#dictionary comprehension
+
+dict = {}
+for i in range(1,6):
+    dict[i] = i*2
+print(dict)
+
+dict_2 = {i : i*2 for i in range(1,7)}
+print(dict_2)
+
+countries = ['col', 'mex', 'bol', 'per']
+population = {}
+for country in countries:
+    population[country] = random.randint(1,100)
+
+print(population)
+
+population_2 = {country : random.randint(1,100) for country in countries}
+print(population_2)
+
+names = ['nico', 'juan', 'santi']
+ages = [5, 6, 7]
+print(list(zip(names, ages)))
+
+new_dict = {names:ages for (names,ages) in zip(names, ages)} 
+print(new_dict)
+'''
+countries = ['col', 'mex', 'bol', 'per']
+population_2 = {country : random.randint(1,100) for country in countries}
+print(population_2)
+
+result = {country : population for (country, population) in population_2.items() if population > 20}
+print(result)
+cont = 0
+text = 'Hola, soy jeferson'
+count = []
+unique = {c : 0 for c in text if c in 'aeiou'}
+print(unique)
+for c in text:
+    if c in 'aeiou':
+        unique[c] += 1
+print(unique)
+
+vocal = {key : 0 for key in 'aeiou'}
+print(vocal)
+
